65879c259d1691c7c08b404cdcb37d5cf31e471e
adrian ruckli <a.ruckli05@gmail.com>
2020-10-30 15:19:44 +0100

    pass testing part3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map'; // just here so that the tests (using deprecated logic) succeed...
+
 
 @Injectable()
 export class ProductService {
@@ -10,7 +12,9 @@ export class ProductService {
 
   constructor( private _http: Http ) { }
 
-  getAlbum( id: number ): Observable<Response> {
+  //Observable<Response>
+  // cannot define return type yet due to predefined tests...
+  getAlbum( id: number ) {
     return this._http.get( this._albumUrl ).pipe( map( response => response.json() ));
   }
 
