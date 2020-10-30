a0ead9951e7e9caa54095ec8b76a166b0998ad64
adrian ruckli <a.ruckli05@gmail.com>
2020-10-30 14:43:03 +0100

    product service added getAlbum

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
 
-  _albumUrl: String = '.../assets/album.json';
+  _albumUrl: string = '.../assets/album.json';
 
-  constructor(private http: Http) { }
+  constructor( private _http: Http ) { }
+
+  getAlbum( id: number ): any {
+    return this._http.get( this._albumUrl ).pipe( map( response => response.json() ) );
+  }
 
 }
